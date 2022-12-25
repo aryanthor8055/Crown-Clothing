6e62994baef99e5218430f2a2015fdf46cd1eb74
Aryan <aryan16072001236809@gmail.com>
2022-12-25 16:52:00 +0530

    Install React Router and Making Home Component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,37 +1,38 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/Home/home.component";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am Navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+
+}
+const Shop = () => {
+  return (
+    <div>
+      <h1> I am Shop component</h1>
+
+    </div>
+  )
+
+}
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
   return (
-    <Directory categories={categories} />
-  );
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path="home" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
