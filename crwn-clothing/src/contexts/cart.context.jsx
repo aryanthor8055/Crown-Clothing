31b487e6eb4c229671cba56f7b84382b88e28e64
Aryan <aryan16072001236809@gmail.com>
2023-01-16 16:26:07 +0530

    creating a Checkout-item Component and adding remove-AllItem from it

--- crwn-clothing/src/contexts/cart.context.jsx
+++ crwn-clothing/src/contexts/cart.context.jsx
@@ -28,12 +28,18 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     )
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id)
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => { },
     cartItems: [],
     addItemToCart: () => { },
     removeItemFromCart: () => { },
+    clearItemFromCart: () => { },
+    cartCount: 0
 
 })
 
@@ -55,7 +61,10 @@ export const CartProvider = ({ children }) => {
     const removeItemToCart = (cartItemToRemove) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove))
     }
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, cartItems, cartCount }
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear))
+    }
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, cartItems, cartCount, clearItemFromCart }
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
