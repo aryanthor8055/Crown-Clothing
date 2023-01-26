760481244b43f9e3f46268e3fd90589e0b5a6447
Aryan <aryan16072001236809@gmail.com>
2023-01-26 21:04:29 +0530

    Changing Form Input styles to jsx format and Making Links to Home Categories

--- crwn-clothing/src/components/directory-item/directory-item.component.jsx
+++ crwn-clothing/src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-    const { title, imageUrl } = category;
+    const { title, imageUrl, route } = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImage
                 imageUrl={imageUrl} />
             <Body>
--- crwn-clothing/src/contexts/categories.context.jsx
+++ crwn-clothing/src/contexts/categories.context.jsx
@@ -19,7 +19,7 @@ export const CategoriesProvider = ({ children }) => {
     useEffect(() => {
         const getCategoriesMap = async () => {
             const categoryMap = await getCategoriesAndDocuments()
-            console.log(categoryMap);
+
             setcategoriesMap(categoryMap);
         }
         getCategoriesMap()
