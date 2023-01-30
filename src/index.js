78d767aebf7731cf5c908a6c5958729829a6aaba
Aryan <aryan16072001236809@gmail.com>
2023-01-30 20:29:51 +0530

    Installing Redux and Initializing Store for populating user and cart instead of Context Api

--- src/index.js
+++ src/index.js
@@ -3,27 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
 
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
 
-    </BrowserRouter>
-
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
